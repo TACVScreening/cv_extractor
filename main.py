996243a76a3459174682a41d60254ea1be621e19
Rizky Akbar Asmaran <88304248+kibare@users.noreply.github.com>
2024-07-28 01:55:36 +0700

    feat: cors middleware

--- main.py
+++ main.py
@@ -14,6 +14,18 @@ from pdf2image.exceptions import (
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
